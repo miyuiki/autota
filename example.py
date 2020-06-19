229886ac104d103b5488ec864407155a7f489132
張覺意 <a121406@gmail.com>
2020-06-19 21:20:45 +0800

    add readme

--- example.py
+++
@@ -1,5 +0,0 @@
-from autota.grader import Grader
-from autota.recommender import Recommender
-
-r = Recommender(pdf_path='./test.pdf', num_page=2, api_port=5555, api_url='140.115.53.158')
-print(r.guiding_from(ta_ans='物件的內容透過等號來做指派'))
